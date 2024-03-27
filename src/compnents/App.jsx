60edad682f217a2c83e0d9e06ef2c635b9aad62b
Ketan16379 <ketanmeshram184@gmail.com>
2024-03-27 22:51:33 +0530

    Initial Commit

---
+++ src/compnents/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import  { useState } from "react";
+
+function App(){
+    const [name, setName] = useState("");
+    const [heading, setHeading] = useState("");
+
+    function handleChange(){
+        setName(event.target.value);
+    }
+    function handleClick(){
+        setHeading(name);
+    }
+    return (
+        <div className="container">
+          <h1>Hello {heading}</h1>
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="What's your name?"
+            value={name}
+          />
+          <button onClick={handleClick}>Submit</button>
+        </div>
+      );
+}
+
+export default App;
